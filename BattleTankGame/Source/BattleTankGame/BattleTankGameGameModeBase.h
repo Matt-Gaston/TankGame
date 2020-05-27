ed89069a90d6fef1d20e2db503db361c2d5f0a40
Matthew Gaston <19mattgaston@gmail.com>
2020-05-26 19:27:27 -0500

    Created empty project in unreal. Added .vscode/ to gitignore

---
+++ BattleTankGame/Source/BattleTankGame/BattleTankGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BattleTankGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANKGAME_API ABattleTankGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
